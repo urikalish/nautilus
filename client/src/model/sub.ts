149b990d25bdb22a2e0b55965baf60ae0a86caf4
kalish <ukalish@opentext.com>
2025-02-05 14:12:51 +0200

    wip

--- client/src/model/sub.ts
+++ client/src/model/sub.ts
@@ -1,5 +1,6 @@
 import { Position } from './position';
 import { getRandomNumber } from '../services/utils';
+import { Contact } from './contact';
 
 export class Sub {
 	id: number;
@@ -9,6 +10,7 @@ export class Sub {
 	rotation: number; //degrees
 	speed: number; //knots
 	depth: number; //feet
+	contacts: Contact[];
 
 	constructor(index: number, position: Position, course: number, speed: number, depth: number) {
 		this.id = getRandomNumber(6);
@@ -18,5 +20,6 @@ export class Sub {
 		this.rotation = course <= 180 ? course : course - 360;
 		this.speed = speed;
 		this.depth = depth;
+		this.contacts = [];
 	}
 }
