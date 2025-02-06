e5e851592240e7a699d2240672df6a90d3f8b7e4
kalish <ukalish@opentext.com>
2025-02-06 09:29:15 +0200

    wip

--- client/src/model/settings.ts
+++ client/src/model/settings.ts
@@ -8,7 +8,7 @@ export const settings = {
 		standard: 25, //knots
 		full: 30, //knots
 		flank: 40, //knots
-		changePerSec: 1, //knots
+		changePerSec: 3, //knots
 	},
 	depth: {
 		test: 800, //feet
